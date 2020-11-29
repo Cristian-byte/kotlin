2e4c197d6e4ddfaf6f9b82342f504a9d599f2dbf
Cristian-byte <andygign40@gmail.com>
2020-11-29 03:18:51 -0600

    Configurando la App

--- app/src/main/java/com/platzi/conf/view/adapter/SpeakerListener.kt
+++ app/src/main/java/com/platzi/conf/view/adapter/SpeakerListener.kt
@@ -3,5 +3,7 @@ package com.platzi.conf.view.adapter
 import com.platzi.conf.model.Speaker
 
 interface SpeakerListener {
+    abstract var layoutManager: GridLayoutManager
+
     fun onSpeakerClicked(speaker: Speaker, position: Int)
 }
