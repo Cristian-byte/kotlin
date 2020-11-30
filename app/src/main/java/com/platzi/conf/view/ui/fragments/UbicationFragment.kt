898e0045e21ce0885abe171ff0271fa9dff50904
Cristian-byte <andygign40@gmail.com>
2020-11-30 00:19:06 -0600

    Colocar Estiloes e Informacion

--- app/src/main/java/com/platzi/conf/view/ui/fragments/UbicationFragment.kt
+++ app/src/main/java/com/platzi/conf/view/ui/fragments/UbicationFragment.kt
@@ -1,5 +1,6 @@
 package com.platzi.conf.view.ui.fragments
 
+
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
@@ -8,36 +9,22 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.BitmapDescriptorFactory
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MapStyleOptions
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 import com.platzi.conf.R
 import com.platzi.conf.model.Ubication
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
- * Use the [UbicationFragment.newInstance] factory method to
- * create an instance of this fragment.
  */
-class UbicationFragment : Fragment() {
-    private var param1: String? = null
-    private var param2: String? = null
+class UbicationFragment : Fragment(), OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_ubication, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -47,7 +34,7 @@ class UbicationFragment : Fragment() {
         mapFragment.getMapAsync(this)
     }
 
-    fun onMapReady(googleMap: GoogleMap?) {
+    override fun onMapReady(googleMap: GoogleMap?) {
         val ubication = Ubication()
 
         val zoom = 16f
@@ -68,32 +55,12 @@ class UbicationFragment : Fragment() {
         googleMap?.addMarker(markerOptions)
         googleMap?.setOnMarkerClickListener(this)
 
-        googleMap?.setMapStyle(MapStyleOptions.loadRawResourceStyle( ) )
+        googleMap?.setMapStyle(MapStyleOptions.loadRawResourceStyle(context, R.raw.custom_map))
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ubication, container, false)
+    override fun onMarkerClick(p0: Marker?): Boolean {
+        findNavController().navigate(R.id.ubicationDetailFragment)
+        return true
     }
-
-    companion object {
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            UbicationFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
-}
-
-private fun SupportMapFragment.getMapAsync(UbicationFragment: UbicationFragment) {
 
 }
-
-private fun GoogleMap?.setOnMarkerClickListener(UbicationFragment: UbicationFragment: UbicationFragment): Any =
-    Unit
